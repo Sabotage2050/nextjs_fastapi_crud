6d83e6faf3afc39fecb5a96878c50c4e50785c45
removable4463 <removable4463@gmail.com>
2023-12-22 14:02:11 +0900

    datetime削除

--- backend/dev/nextjs_fastapi_crud_backend_api/nextjs_fastapi_crud_backend_api/cruds/todo.py
+++ backend/dev/nextjs_fastapi_crud_backend_api/nextjs_fastapi_crud_backend_api/cruds/todo.py
@@ -1,12 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from nextjs_fastapi_crud_backend_api.models.todo import Item
-from nextjs_fastapi_crud_backend_api.schemas.todo import ItemCreate
-from datetime import datetime
 
 
 async def create_item(db: AsyncSession, request) -> Item:
-    current_time = datetime.now()
     item = Item(
         name=request.name,
         contents=request.contents,
